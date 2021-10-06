3e68ac2d5a450a4a9a5006161d874ada3685bd81
nick <nicolasjsim@gmail.com>
2021-10-06 10:10:44 -0700

    phase 0 and 1

--- react-hooks-pokedex-starter/src/components/PokemonBrowser.js
+++ react-hooks-pokedex-starter/src/components/PokemonBrowser.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Route, useParams } from 'react-router-dom';
-
+import { getPokemon } from '../store/pokemon';
 import PokemonDetail from './PokemonDetail';
 import CreatePokemonForm from './CreatePokemonForm';
 import Fab from './Fab';
@@ -13,9 +13,15 @@ const PokemonBrowser = () => {
   });
   const [showForm, setShowForm] = useState(false);
 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPokemon());
+
+  })
   if (!pokemon) {
     return null;
   }
+
 
   return (
     <main>
